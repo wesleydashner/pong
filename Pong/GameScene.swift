fdd7b816e219b5807d5badade698d0aed154ea89
Wesley Dashner <wdashner11@gmail.com>
2019-05-03 11:44:31 -0600

    added ball and paddle nodes

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -9,6 +9,13 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory {
+    
+    static let ball: UInt32 = 0x1 << 1
+    static let paddles: UInt32 = 0x1 << 2
+    
+}
+
 class GameScene: SKScene {
     
     var ball = SKSpriteNode()
@@ -16,19 +23,61 @@ class GameScene: SKScene {
     var enemyPaddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        ball = SKSpriteNode(imageNamed: "ball")
+        ball.setScale(0.3)
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.width / 2)
+        ball.physicsBody?.categoryBitMask = PhysicsCategory.ball
+        ball.physicsBody?.collisionBitMask = PhysicsCategory.paddles
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.paddles
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.isDynamic = true
+        self.addChild(ball)
         
+        playerPaddle = SKSpriteNode(imageNamed: "paddle")
+        playerPaddle.setScale(0.2)
+        playerPaddle.position = CGPoint(x: 0, y: -self.frame.height / 2 + 50)
+        playerPaddle.physicsBody = SKPhysicsBody(rectangleOf: playerPaddle.size)
+        playerPaddle.physicsBody?.categoryBitMask = PhysicsCategory.paddles
+        playerPaddle.physicsBody?.collisionBitMask = PhysicsCategory.ball
+        playerPaddle.physicsBody?.contactTestBitMask = PhysicsCategory.ball
+        playerPaddle.physicsBody?.affectedByGravity = false
+        playerPaddle.physicsBody?.isDynamic = false
+        self.addChild(playerPaddle)
+        
+        enemyPaddle = SKSpriteNode(imageNamed: "paddle")
+        enemyPaddle.setScale(0.2)
+        enemyPaddle.position = CGPoint(x: 0, y: -self.frame.height / 2 + 50)
+        enemyPaddle.physicsBody = SKPhysicsBody(rectangleOf: enemyPaddle.size)
+        enemyPaddle.physicsBody?.categoryBitMask = PhysicsCategory.paddles
+        enemyPaddle.physicsBody?.collisionBitMask = PhysicsCategory.ball
+        enemyPaddle.physicsBody?.contactTestBitMask = PhysicsCategory.ball
+        enemyPaddle.physicsBody?.affectedByGravity = false
+        enemyPaddle.physicsBody?.isDynamic = false
+        self.addChild(enemyPaddle)
+        
+//        ground = SKSpriteNode(imageNamed: "ground")
+//        ground.setScale(0.7)
+//        ground.position = CGPoint(x: 0, y: -(self.frame.height / 2) + ground.frame.height / 2)
+//        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+//        ground.physicsBody?.categoryBitMask = PhysicsCategory.ground
+//        ground.physicsBody?.collisionBitMask = PhysicsCategory.ball
+//        ground.physicsBody?.contactTestBitMask = PhysicsCategory.ball
+//        ground.physicsBody?.affectedByGravity = false
+//        ground.physicsBody?.isDynamic = false
+//        self.addChild(ground)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            
-        }
+//        for t in touches {
+//
+//        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            
-        }
+//        for t in touches {
+//
+//        }
     }
     
 }
