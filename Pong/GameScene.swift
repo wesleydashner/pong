9040fcc8908af617bff8b20a7a131d81b26eee06
Wesley Dashner <wdashner11@gmail.com>
2019-05-03 11:10:06 -0600

    kinda initial commit

---
+++ Pong/GameScene.swift
@@ -0,0 +1,34 @@
+//
+//  GameScene.swift
+//  Pong
+//
+//  Created by Wesley Dashner on 5/3/19.
+//  Copyright © 2019 Wesley Dashner. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var ball = SKSpriteNode()
+    var playerPaddle = SKSpriteNode()
+    var enemyPaddle = SKSpriteNode()
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            
+        }
+    }
+    
+}
